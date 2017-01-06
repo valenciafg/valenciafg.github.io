bc24c729dc4e4f05b030efca3ef5b43b5176b600
Víctor Valencia <valencia6x@gmail.com>
2017-01-06 12:11:36 -0400

    changed toolbar to appbar on navigation menu

--- src/app/components/NavigationMenu.js
+++ src/app/components/NavigationMenu.js
@@ -2,6 +2,10 @@ import React from 'react';
 import MenuItem from 'material-ui/MenuItem';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import rsScroller from 'react-smooth-scroller';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import FlatButton from 'material-ui/FlatButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 export default class NavigationMenu extends React.Component {
     constructor(props) {
@@ -15,16 +19,21 @@ export default class NavigationMenu extends React.Component {
     }
     render(){
         return(
-            <Toolbar style={{position: 'fixed'}}>
-                <ToolbarTitle text="Víctor Valencia" />
-                <ToolbarSeparator />
-                <a onClick={this.onScrollTop} href="#main">Home</a>
-                <a onClick={() => this.onScrollTarget('about')}>About</a>
-                <a onClick={() => this.onScrollTarget('knowledge')}>Knowledge</a>
-                <a onClick={() => this.onScrollTarget('portafolio')}>Portafolio</a>
-                <a onClick={() => this.onScrollTarget('experience')}>Experience</a>
-                <a onClick={() => this.onScrollTarget('contact')}>Contact</a>
-            </Toolbar>
+            <AppBar
+            title={<span>Víctor Valencia</span>}
+            style={{position: 'fixed'}}
+            iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+            iconElementRight={
+                <div>
+                <FlatButton label="Home" onClick={this.onScrollTop}/>
+                <FlatButton label="About" onClick={() => this.onScrollTarget('about')}/>
+                <FlatButton label="Knowledge" onClick={() => this.onScrollTarget('knowledge')}/>
+                <FlatButton label="Portafolio" onClick={() => this.onScrollTarget('portafolio')}/>
+                <FlatButton label="Experience" onClick={() => this.onScrollTarget('experience')}/>
+                <FlatButton label="Contact" onClick={() => this.onScrollTarget('contact')}/>
+                </div>
+            }
+            />
         );
     }
 }
