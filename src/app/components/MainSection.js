0c654b438959a4588c3ee68d72f003917b827ac8
Víctor Valencia <valencia6x@gmail.com>
2017-01-12 15:10:34 -0400

    test profile picture

--- src/app/components/MainSection.js
+++ src/app/components/MainSection.js
@@ -11,7 +11,7 @@ export default class MainSection extends React.Component {
             <div className={id}>
                 <Segment vertical textAlign='center' padded='very'>
                     <Header as='h1' icon textAlign='center'>
-                        <Image src='dist/images/tati.png' size='small' shape='circular' alt="profile picture" />
+                        <Image src='dist/images/tati.jpg' size='small' shape='circular' alt="profile picture" />
                         <Header.Content>
                             Víctor Valencia
                             <Header.Subheader>
